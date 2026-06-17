908db3a5c8112d2075a7cf465f0e2550626556eb
Hafiz Sameer <hafizsameer601@gmail.com>
2026-06-17 22:34:26 +0500

    Improved the Java user interface

--- src/LoginFrame.java
+++ src/LoginFrame.java
@@ -12,27 +12,53 @@ public class LoginFrame extends JFrame implements ActionListener {
     public LoginFrame() {
 
         setTitle("SwiftAid - Login");
-        setSize(400, 250);
+        setSize(450, 300);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel panel = new JPanel(new GridLayout(3, 2, 10, 10));
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 
-        panel.add(new JLabel("Username"));
-        usernameField = new JTextField();
-        panel.add(usernameField);
+        JLabel titleLabel = new JLabel("SwiftAid Login");
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 22));
+        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        panel.add(new JLabel("Password"));
-        passwordField = new JPasswordField();
-        panel.add(passwordField);
+        mainPanel.add(Box.createVerticalStrut(20));
+        mainPanel.add(titleLabel);
+        mainPanel.add(Box.createVerticalStrut(20));
 
-        panel.add(new JLabel());
+        JPanel usernamePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+        JLabel usernameLabel = new JLabel("Username: ");
+
+        usernameField = new JTextField(15);
+
+        usernamePanel.add(usernameLabel);
+        usernamePanel.add(usernameField);
+
+        mainPanel.add(usernamePanel);
+
+        JPanel passwordPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+        JLabel passwordLabel = new JLabel("Password: ");
+
+        passwordField = new JPasswordField(15);
+
+        passwordPanel.add(passwordLabel);
+        passwordPanel.add(passwordField);
+
+        mainPanel.add(passwordPanel);
+
+        mainPanel.add(Box.createVerticalStrut(15));
 
         loginButton = new JButton("Login");
+        loginButton.setPreferredSize(new Dimension(120, 35));
         loginButton.addActionListener(this);
-        panel.add(loginButton);
+        loginButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        add(panel);
+        mainPanel.add(loginButton);
+
+        add(mainPanel);
 
         setVisible(true);
     }
@@ -43,8 +69,8 @@ public class LoginFrame extends JFrame implements ActionListener {
         String username = usernameField.getText();
         String password = String.valueOf(passwordField.getPassword());
 
-        if(username.equals("admin") &&
-                password.equals("admin123")) {
+        if (username.equals("admin")
+                && password.equals("admin123")) {
 
             JOptionPane.showMessageDialog(
                     this,
